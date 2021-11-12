c6b3573b584585cf1e36b2bbbf961ee0605ddda3
John <john@gis.tw>
2021-11-12 15:04:53 +0800

    簽入 watchdog 初步完成

--- system_status_watchdog/system_status_watchdog/App_Code/include.cs
+++ system_status_watchdog/system_status_watchdog/App_Code/include.cs
@@ -1,12 +1,433 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.Cache;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace system_status_watchdog.App_Code
+namespace utility
 {
-    class include
+    public class myinclude
     {
+
+        public string pwd()
+        {
+            //return Directory.GetCurrentDirectory();
+            return System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+        }
+        public bool is_file(string filepath)
+        {
+            return File.Exists(filepath);
+        }
+        public byte[] file_get_contents(string url)
+        {
+            if (url.ToLower().IndexOf("http:") > -1 || url.ToLower().IndexOf("https:") > -1)
+            {
+                // URL                 
+                ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+                HttpWebRequest request = null;
+                HttpWebResponse response = null;
+                byte[] byteData = null;
+
+                request = (HttpWebRequest)WebRequest.Create(url);
+                request.Timeout = 60000;
+                request.Proxy = null;
+                request.UserAgent = "user_agent','Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)";
+                //request.Referer = getSystemKey("HTTP_REFERER");
+                response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                byteData = ReadStream(stream, 32765);
+                response.Close();
+                stream.Close();
+                return byteData;
+            }
+            else
+            {
+                /*System.IO.StreamReader sr = new System.IO.StreamReader(url);
+                
+                string sContents = sr.ReadToEnd();
+                sr.Close();
+                return s2b(sContents);
+                */
+                /*FileStream fs = new FileStream(url, FileMode.Open);
+                byte[] buffer = new byte[fs.Length];
+                fs.Read(buffer, 0, buffer.Length);
+                fs.Close();
+                return buffer;
+                */
+                byte[] data;
+
+                using (var fs = new FileStream(url, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    data = ReadStream(fs, 8192);
+                    /*using (StreamReader sr = new StreamReader(fs))
+                    {
+                        data = ReadStream(sr, 8192);
+                        /*using (MemoryStream ms = new MemoryStream())
+                        {
+                            sr.BaseStream.CopyTo(ms);
+                            data = ms.ToArray();
+                            ms.Close();
+                            ms.Dispose();
+                        }
+                        */
+                    //sr.Close();
+                    //sr.Dispose();
+
+                    //};
+                    fs.Close();
+                };
+
+                return data;
+            }
+        }
+        private byte[] ReadStream(Stream stream, int initialLength)
+        {
+            if (initialLength < 1)
+            {
+                initialLength = 32768;
+            }
+            byte[] buffer = new byte[initialLength];
+            int read = 0;
+            int chunk;
+            while ((chunk = stream.Read(buffer, read, buffer.Length - read)) > 0)
+            {
+                read += chunk;
+                if (read == buffer.Length)
+                {
+                    int nextByte = stream.ReadByte();
+                    if (nextByte == -1)
+                    {
+                        return buffer;
+                    }
+                    byte[] newBuffer = new byte[buffer.Length * 2];
+                    Array.Copy(buffer, newBuffer, buffer.Length);
+                    newBuffer[read] = (byte)nextByte;
+                    buffer = newBuffer;
+                    read++;
+                }
+            }
+            byte[] bytes = new byte[read];
+            Array.Copy(buffer, bytes, read);
+            return bytes;
+        }
+        public string b2s(byte[] input)
+        {
+            return System.Text.Encoding.UTF8.GetString(input);
+        }
+        public byte[] s2b(string input)
+        {
+            return System.Text.Encoding.UTF8.GetBytes(input);
+        }
+
+        public string getSystemKey(string keyindex)
+        {
+            return System.Configuration.ConfigurationManager.AppSettings[keyindex];
+        }
+        public string trim(string input)
+        {
+            return input.Trim();
+        }
+        public JArray json_decode(string input)
+        {
+            input = trim(input);
+            if (input.Length != 0)
+            {
+                if (input.Substring(1, 1) != "[")
+                {
+                    input = "[" + input + "]";
+                    return (JArray)JsonConvert.DeserializeObject<JArray>(input);
+                }
+                else
+                {
+                    return (JArray)JsonConvert.DeserializeObject<JArray>(input);
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public string mainname(string path)
+        {
+            return Path.GetFileNameWithoutExtension(path);
+        }
+        public void killProcess(string processName)
+        {
+            string mn = mainname(processName);
+            foreach (var process in Process.GetProcessesByName(mn))
+            {
+                process.Kill();
+            }
+        }
+        public void file_put_contents(string filepath, string input)
+        {
+            file_put_contents(filepath, s2b(input), false);
+        }
+        public void file_put_contents(string filepath, byte[] input)
+        {
+            file_put_contents(filepath, input, false);
+        }
+        public void file_put_contents(string filepath, string input, bool isFileAppend)
+        {
+            file_put_contents(filepath, s2b(input), isFileAppend);
+        }
+        public void file_put_contents(string filepath, byte[] input, bool isFileAppend)
+        {
+
+            switch (isFileAppend)
+            {
+                case true:
+                    {
+                        FileMode FM = new FileMode();
+                        if (!is_file(filepath))
+                        {
+                            FM = FileMode.Create;
+                            using (FileStream myFile = File.Open(@filepath, FM, FileAccess.Write, FileShare.Read))
+                            {
+                                myFile.Seek(myFile.Length, SeekOrigin.Begin);
+                                myFile.Write(input, 0, input.Length);
+                                myFile.Dispose();
+                            }
+                        }
+                        else
+                        {
+                            FM = FileMode.Append;
+                            using (FileStream myFile = File.Open(@filepath, FM, FileAccess.Write, FileShare.Read))
+                            {
+                                myFile.Seek(myFile.Length, SeekOrigin.Begin);
+                                myFile.Write(input, 0, input.Length);
+                                myFile.Dispose();
+                            }
+                        }
+
+
+                    }
+                    break;
+                case false:
+                    {
+                        using (FileStream myFile = File.Open(@filepath, FileMode.Create, FileAccess.ReadWrite, FileShare.Read))
+                        {
+                            myFile.Write(input, 0, input.Length);
+                            myFile.Dispose();
+                        };
+                    }
+                    break;
+            }
+        }
+        public string system(string command)
+        {
+            StringBuilder sb = new StringBuilder();
+            string version = System.Environment.OSVersion.VersionString;//读取操作系统版本  
+            if (version.Contains("Windows"))
+            {
+                using (Process p = new Process())
+                {
+                    p.StartInfo.FileName = "cmd.exe";
+
+                    p.StartInfo.UseShellExecute = false;//是否指定操作系统外壳进程启动程序  
+                    p.StartInfo.RedirectStandardInput = true;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = true;//不显示dos命令行窗口  
+
+                    p.Start();//启动cmd.exe  
+                    p.StandardInput.WriteLine(command);//输入命令  
+                    p.StandardInput.WriteLine("exit");//退出cmd.exe  
+                    p.WaitForExit();//等待执行完了，退出cmd.exe  
+
+                    using (StreamReader reader = p.StandardOutput)//截取输出流  
+                    {
+                        string line = reader.ReadLine();//每次读取一行  
+                        line = line.Replace("(c) 2018 Microsoft Corporation. 著作權所有，並保留一切權利。", "");
+                        line = line.Replace("Microsoft Windows [版本 10.0.17134.1488]", "");
+                        line = line.Replace("Microsoft Windows [版本 10.0.19041.450]", "");
+                        line = line.Replace("(c) 2020 Microsoft Corporation。著作權所有，並保留一切權利。", "");
+                        line = line.Trim();
+                        while (!reader.EndOfStream)
+                        {
+                            sb.Append(line).Append("\n");//在Web中使用<br />换行  
+                            line = reader.ReadLine();
+                            line = line.Replace("(c) 2018 Microsoft Corporation. 著作權所有，並保留一切權利。", "");
+                            line = line.Replace("Microsoft Windows [版本 10.0.17134.1488]", "");
+                            line = line.Replace("Microsoft Windows [版本 10.0.19041.450]", "");
+                            line = line.Replace("(c) 2020 Microsoft Corporation。著作權所有，並保留一切權利。", "");
+                            line = line.Trim();
+                        }
+                        p.WaitForExit();//等待程序执行完退出进程  
+                        p.Close();//关闭进程  
+                        reader.Close();//关闭流  
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+        public bool isProcessRunning(string processName)
+        {
+            bool isRunning = false;
+            string mn = mainname(processName);
+            foreach (var process in Process.GetProcessesByName(mn))
+            {
+                isRunning = true;
+                break;
+            }
+            return isRunning;
+        }
+        public bool is_file_lock(string filePath)
+        {
+            try
+            {
+                using (File.Open(filePath, FileMode.Open)) { }
+            }
+            catch (IOException e)
+            {
+                var errorCode = Marshal.GetHRForException(e) & ((1 << 16) - 1);
+                return errorCode == 32 || errorCode == 33;
+            }
+            return false;
+        }
+        public bool is_dir(string path)
+        {
+            return Directory.Exists(path);
+        }
+        public void mkdir(string path)
+        {
+            try
+            {
+                Directory.CreateDirectory(path);
+            }
+            catch
+            {
+            }
+        }
+        public DateTime UnixTimeToDateTime(string text)
+        {
+            System.DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+            // Add the number of seconds in UNIX timestamp to be converted.            
+            dateTime = dateTime.AddSeconds(Convert.ToDouble(text));
+            return dateTime;
+        }
+        public string date(string format, string unixtimestamp)
+        {
+            DateTime tmp = UnixTimeToDateTime(unixtimestamp);
+            tmp = tmp.AddHours(+8);
+            switch (format)
+            {
+                case "Ymd":
+                    return tmp.ToString("yyyyMMdd");
+                case "Ym":
+                    return tmp.ToString("yyyyMM");
+                case "YmdHis":
+                    return tmp.ToString("yyyyMMddHHmmss");
+                case "Y-m-d H:i:s":
+                    return tmp.ToString("yyyy-MM-dd HH:mm:ss");
+                case "Y/m/d":
+                    return tmp.ToString("yyyy/MM/dd");
+                case "Y/m/d H:i:s":
+                    return tmp.ToString("yyyy/MM/dd HH:mm:ss");
+                case "Y/m/d H:i:s.fff":
+                    return tmp.ToString("yyyy/MM/dd HH:mm:ss.fff");
+                case "Y-m-d_H_i_s":
+                    return tmp.ToString("yyyy-MM-dd_HH_mm_ss");
+                case "Y-m-d":
+                    return tmp.ToString("yyyy-MM-dd");
+                case "md":
+                    return tmp.ToString("MMdd");
+                case "H:i:s":
+                    return tmp.ToString("HH:mm:ss");
+                case "H:i":
+                    return tmp.ToString("HH:mm");
+                case "Y-m-d H:i":
+                    return tmp.ToString("yyyy-MM-dd HH:mm");
+                case "Y_m_d_H_i_s":
+                    return tmp.ToString("yyyy_MM_dd_HH_mm_ss");
+                case "Y_m_d_H_i_s_fff":
+                    return tmp.ToString("yyyy_MM_dd_HH_mm_ss_fff");
+                case "w":
+                    //回傳week, sun =0 , sat = 6, mon=1.....
+                    return Convert.ToInt16(tmp.DayOfWeek).ToString();
+                case "Y":
+                    return tmp.ToString("yyyy");
+                case "m":
+                    return tmp.ToString("MM");
+                case "d":
+                    return tmp.ToString("dd");
+                case "H":
+                    return tmp.ToString("HH");
+                case "i":
+                    return tmp.ToString("mm");
+                case "s":
+                    return tmp.ToString("ss");
+                case "Y-m-d H:i:s.fff":
+                    return tmp.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                case "Y-m-d H:i:s.ffffff":
+                    return tmp.ToString("yyyy-MM-dd HH:mm:ss.ffffff");
+                case "H:i:s.fff":
+                    return tmp.ToString("HH:mm:ss.fff");
+                case "H:i:s.ffffff":
+                    return tmp.ToString("HH:mm:ss.ffffff");
+            }
+            return "";
+        }
+        public bool is_string_like(string data, string find_string)
+        {
+            return (data.IndexOf(find_string) == -1) ? false : true;
+        }
+        public bool is_istring_like(string data, string find_string)
+        {
+            return (data.ToUpper().IndexOf(find_string.ToUpper()) == -1) ? false : true;
+        }
+        //strtotime 轉換成 Unix time
+        public string strtotime(string value)
+        {
+            //create Timespan by subtracting the value provided from
+            //the Unix Epoch
+            TimeSpan span = (Convert.ToDateTime(value) - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime());
+
+            //return the total seconds (which is a UNIX timestamp)
+            if (is_string_like(value, "."))
+            {
+                //有小數點               
+                double sec = span.Ticks / (TimeSpan.TicksPerMillisecond / 1000.0) / 1000000.0;
+                return sec.ToString();
+            }
+            else
+            {
+                return span.TotalSeconds.ToString();
+            }
+        }
+        public string strtotime(DateTime value)
+        {
+            //create Timespan by subtracting the value provided from
+            //the Unix Epoch
+            TimeSpan span = (value - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime());
+
+            //return the total seconds (which is a UNIX timestamp)
+            return span.TotalSeconds.ToString();
+        }
+
+        public string time()
+        {
+            return strtotime(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+        public string date()
+        {
+            return date("Y-m-d H:i:s", strtotime(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffffff")));
+        }
+        public string date(string format)
+        {
+            return date(format, strtotime(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffffff")));
+        }
     }
 }
--- system_status_watchdog/system_status_watchdog/Program.cs
+++ system_status_watchdog/system_status_watchdog/Program.cs
@@ -1,17 +1,140 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-
+using utility;
+using System.Runtime.InteropServices;
 namespace system_status_watchdog
 {
-    class Program
+
+    public class Program
     {
-        public Dictionary<string, Thread> threads = new Dictionary<string, Thread>();
+        static public Program F1 = null;
+        public string PWD = null;
+        public myinclude my = new myinclude();
+        Dictionary<string, Thread> threads = new Dictionary<string, Thread>();
+        string LOG_PATH = null;
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+        private void create_log_dir()
+        {
+            if (!my.is_dir(F1.LOG_PATH))
+            {
+                my.mkdir(F1.LOG_PATH);
+            }
+        }
+
+        public void logError(string data)
+        {
+            try
+            {
+                if (!my.is_dir(LOG_PATH))
+                {
+                    my.mkdir(LOG_PATH);
+                }
+                my.file_put_contents(LOG_PATH + "\\" + my.date("Y-m-d") + ".txt", my.date("Y-m-d H:i:s") + ":\r\n" + data + "\r\n", true);
+            }
+            catch
+            {
+
+            }
+        }
+        private void myCrash(object sender, UnhandledExceptionEventArgs args)
+        {
+            System.Environment.Exit(0);
+        }
+        public void run_check_update()
+        {
+            //檢查目前版本，如果版本檔不存在，關程式，下載新版，執行      
+            double local_version = 0;
+            double remote_version = 0;
+            bool isNeedUpdate = false;
+            string downloadPath = "";
+            //Console.WriteLine("step 1");
+            if (!F1.my.is_file(F1.PWD + "\\version.txt"))
+            {
+                isNeedUpdate = true;
+                //Console.WriteLine("step 2");
+            }
+            else
+            {
+                //檢查現在版本，跟網站上版本
+                local_version = Convert.ToDouble(F1.my.b2s(F1.my.file_get_contents(F1.PWD + "\\version.txt")));
+                remote_version = local_version;
+                try
+                {
+                    var jd = my.json_decode(F1.my.b2s(my.file_get_contents(F1.my.getSystemKey("checkVersionURL"))));
+                    remote_version = Convert.ToDouble(jd[0]["version"].ToString());
+                    downloadPath = jd[0]["downloadPath"].ToString();
+                    //Console.WriteLine("step 3");
+                }
+                catch
+                {
+                    //無網路
+                }
+            }
+            if (local_version != remote_version || isNeedUpdate)
+            {
+                //Console.WriteLine("step 4");
+                //版本不一樣，如果服務正在跑，就停止，下載，重新啟動程式
+                F1.my.killProcess("system_status");
+                if (downloadPath != "")
+                {
+                    //Console.WriteLine("step 5");
+                    F1.my.file_put_contents(F1.PWD + "\\system_status.exe", F1.my.file_get_contents(downloadPath));
+                    F1.logError("下載更新版本...：" + remote_version.ToString());
+                }
+                F1.my.system(F1.PWD + "\\system_status.exe");
+            }
+            if (!F1.my.isProcessRunning("system_status"))
+            {
+                //Console.WriteLine("step 6");
+                F1.my.system(F1.PWD + "\\system_status.exe");
+            }
+        }
         static void Main(string[] args)
         {
+            // Hide
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            F1 = new Program();
+            F1.PWD = F1.my.pwd();
+            string lockFile = F1.PWD + "\\lock_watchdog.txt";
+            if (!F1.my.is_file(lockFile))
+            {
+                F1.my.file_put_contents(lockFile, "");
+            }
+            if (F1.my.is_file_lock(lockFile))
+            {
+                //如果目前已是 lock 就結束
+                System.Environment.Exit(0);
+                return;
+            }
+            F1.LOG_PATH = F1.PWD + "\\log";
+            F1.create_log_dir();
+            //lock file
+            FileStream s2 = new FileStream(lockFile, FileMode.Open, FileAccess.Read, FileShare.None);
+            //嘗試當掉就中斷離開
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(F1.myCrash);
+            while (true)
+            {
+                F1.threads["mainThread"] = new Thread(F1.run_check_update);
+                F1.threads["mainThread"].Start();
+                Thread.Sleep(60 * 1000);
+            }
         }
     }
 }
